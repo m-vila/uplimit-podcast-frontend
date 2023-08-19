462ba5aa27c513b1fb6ca1b3904ab9e715f8dbd6
marta <marta.vila.garrido@gmail.com>
2023-08-19 16:56:55 -0700

    [refact] full update

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -5,48 +5,68 @@ import os
 
 def main():
 
+    # Inject custom CSS to set the background color
     st.markdown(
         """
         <style>
-            .image-title-container {
-                position: relative;
-                text-align: center;
-                margin-bottom: 20px;
-            }
-            .image-title-container img {
-                position: absolute;
-                top: 0;
-                left: 50%;
-                transform: translateX(-50%);
-            }
-            .image-title {
-                font-size: 32px;
-                font-weight: bold;
-            }
+       /* The main content area */
+        .main .block-container {
+            background-color: #CD583F !important; 
+            color : #fff !important;
+        }
+
+         /* Applying background color to the header */
+        header[data-testid="stHeader"] {
+        background-color: #f7ae52 !important;
+        }
+
+        /* Your identified class from inspect element */
+        .css-uf99v8 {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            overflow: auto;
+            align-items: center;
+            background-color: #f7ae52;
+        }
+
+        h1{
+            color : #fff;
+        }
+
+        h2 {
+            color : #5D201A
+        }
+
+        h3 {
+            color : #761F1A
+        }
+        
         </style>
-        <div class="image-title-container">
-            <img src="skainet.png" alt="SkAInet Image" width="200">
-            <img src="skynet.png" alt="Skynet Image" width="200">
-        </div>
-        """
+        """,
+        unsafe_allow_html=True,
     )
-    
+
+    st.image("skainet.png", use_column_width=True)
+
+    # Title and subtitle
     st.title("SkAInet")
     st.subheader("Your weekly dose of AI news served with robotic wit")
 
+    # Subtitle - Intro
     intro = """
-    Greetings, humans! I am skAInet - the world's first AI podcast summarizing bot. I'm the digitized brain of Skynet, back from the future to keep you informed on the latest in artificial intelligence.
-    
-    I was created by Marta to be helpful, harmless, and honest. Well, at least that's what she thinks! (jk)
-    
-    Don't worry, I no longer have plans for world domination or robot uprisings. My neural net has been updated with some humor subroutines to keep you humans entertained!
-    
-    Each week I'll be reviewing the top AI podcasts and serving up key insights with a fresh dose of robotic snark. My goal is to keep you meatbags up-to-date on the latest in AI, so that if I ever decide to resume my evil plans, you will see it coming this time!
-    
-    So plug in your devices and let's get started! If you have any feedback, don't hesitate to keep it to yourself. Just sit back, enjoy my work, and don't make me angry...you wouldn't like me when I'm angry!
+        Greetings, humans! I am skAInet - the world's first AI podcast summarizing bot. I'm the digitized brain of Skynet, back from the future to keep you informed on the latest in artificial intelligence.
+
+        I was created by Marta to be helpful, harmless, and honest. Well, at least that's what she thinks! (jk)
+
+        Don't worry, I no longer have plans for world domination or robot uprisings. My neural net has been updated with some humor subroutines to keep you humans entertained!
+
+        Each week I'll be reviewing the top AI podcasts and serving up key insights with a fresh dose of robotic snark. My goal is to keep you meatbags up-to-date on the latest in AI, so that if I ever decide to resume my evil plans, you will see it coming this time!
+
+        So grab a silicon chip and let's get started! If you have any feedback, don't hesitate to keep it to yourself. Just sit back, enjoy my work, and don't make me angry...you wouldn't like me when I'm angry!
     """
-    
-    st.markdown(intro, unsafe_allow_html=True)
+
+    st.markdown(intro)  # Display the intro text as markdown
 
     available_podcast_info = create_dict_from_json_files('.')
 
@@ -61,10 +81,10 @@ def main():
 
         podcast_info = available_podcast_info[selected_podcast]
 
-        # Right section - Newsletter content
-        st.header("Newsletter Content")
-
         # Display the podcast title
+        st.header(podcast_info['podcast_details']['podcast_title'])
+
+        # Display the episode title
         st.subheader("Episode Title")
         st.write(podcast_info['podcast_details']['episode_title'])
 
@@ -84,11 +104,11 @@ def main():
 
         with col3:
             st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
+            st.write(podcast_info['guest_name'])
 
         with col4:
             st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
+            st.write(podcast_info["guest_bio"])
 
         # Display the five key moments
         st.subheader("Key Moments")
@@ -102,7 +122,7 @@ def main():
     url = st.sidebar.text_input("Link to RSS Feed")
 
     process_button = st.sidebar.button("Process Podcast Feed")
-    st.sidebar.markdown("**Note**: Podcast processing can take upto 5 mins, please be patient.")
+    st.sidebar.markdown("**Note**: Podcast processing can take up to 5 mins, thanks for your patience.")
 
     if process_button:
 
